4bdb493bd4f10c3d2af84d20e4a2eeb79f6982e1
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-03-01 16:16:31 +0400

    [API]: Add not found route

--- api/src/entry.ts
+++ api/src/entry.ts
@@ -3,7 +3,7 @@ import httpProxy from 'http-proxy';
 
 import './config/dotenv';
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
 
 const DB_URL = `${DB_HOST}:${DB_PORT}/_db/${DB_NAME}/`;
 const FoxxProxy = httpProxy.createProxyServer({
@@ -13,10 +13,22 @@ const FoxxProxy = httpProxy.createProxyServer({
 
 const App = express();
 
-App.all('/*', (req, res): void => {
+const proxyResult = (req, res): void => {
   console.log(`redirecting to Foxx ${req.path}`);
 
   FoxxProxy.web(req, res);
+};
+
+App.all('/auth/*', proxyResult);
+App.all('/user/*', proxyResult);
+
+App.all('/*', (_req, res): void => {
+  res.status(404).json({
+    data: null,
+    error: {
+      message: 'route not found',
+    },
+  });
 });
 
 App.listen(3000, () => console.log('Listening to localhost:3000'));
